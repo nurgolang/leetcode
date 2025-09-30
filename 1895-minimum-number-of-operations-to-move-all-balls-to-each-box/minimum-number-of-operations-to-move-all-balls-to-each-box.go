eb98936cd798a25ecb4539c35d24f75391e9ffdd
nurgolang <9029998@gmail.com>
2025-09-30 20:45:07 +0300

    Time: 0 ms (100.00%) | Memory: 7 MB (70.97%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.go
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.go
@@ -1,15 +1,22 @@
 func minOperations(boxes string) []int {
 	res := make([]int, len(boxes))
+	var leftBalls int
+	var leftCost int
+	var rightBalls int
+	var rightCost int
 	for i := 0; i < len(boxes); i++ {
+		res[i] += leftCost
 		if boxes[i]-'0' == 1 {
-			for idx, _ := range res {
-				diff := i - idx
-				if diff < 0 {
-					diff = -diff
-				}
-				res[idx] += diff
-			}
+			leftBalls += 1
 		}
+		leftCost += leftBalls
+	}
+	for i := len(boxes) - 1; i >= 0; i-- {
+		res[i] += rightCost
+		if boxes[i]-'0' == 1 {
+			rightBalls += 1
+		}
+		rightCost += rightBalls
 	}
 	return res
 }
