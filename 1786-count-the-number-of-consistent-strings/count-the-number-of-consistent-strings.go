503e8ce1d850efa9cb2aa0ec59734508ac74a00a
nurgolang <9029998@gmail.com>
2025-10-04 20:26:18 +0400

    Time: 24 ms (84.75%) | Memory: 8.7 MB (83.05%) - LeetSync

---
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.go
@@ -0,0 +1,25 @@
+import "strings"
+
+func countConsistentStrings(allowed string, words []string) int {
+	// create an empty map to store allowed characters
+	allowedChars := make(map[rune]struct{})
+
+	// iterate over each character (rune) in the allowed string
+	for _, char := range allowed {
+		allowedChars[char] = struct{}{}
+	}
+
+	var cnt int
+
+wordLoop:
+	for _, word := range words {
+		for _, letter := range word {
+			_, found := allowedChars[letter]
+			if !found {
+				continue wordLoop
+			}
+		}
+        cnt++
+	}
+    return cnt
+}
