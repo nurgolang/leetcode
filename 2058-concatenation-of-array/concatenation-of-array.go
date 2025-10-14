0e802a48477a9d6d3aa0b3e695fc40a16ecc22ff
nurgolang <9029998@gmail.com>
2025-10-14 13:42:28 +0400

    Time: 1 ms (7.49%) | Memory: 7.3 MB (42.64%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.go
+++ 2058-concatenation-of-array/concatenation-of-array.go
@@ -1,4 +1,7 @@
-func getConcatenation(nums []int) []int {
-    res := append([]int{}, nums...)
-    return append(res, nums...)
-}
+	// Guaranteed O(N) by pre-allocating the final 2*N capacity to avoid reallocations.
+	func getConcatenation(nums []int) []int {
+		res := make([]int, len(nums)*2)
+		copy(res, nums)
+		copy(res[len(nums):], nums)
+		return res
+	}
