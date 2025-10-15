3638dd931db8e81e792c4ce602b6f41fabfc2913
nurgolang <9029998@gmail.com>
2025-10-15 19:42:43 +0400

    Time: 0 ms (100.00%) | Memory: 3.8 MB (75.51%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.go
+++ 2556-convert-the-temperature/convert-the-temperature.go
@@ -1,9 +1,7 @@
 func convertTemperature(celsius float64) []float64 {
-	ans := make([]float64, 2)
 	kelvin := celsius + 273.15
 	fahrenheit := celsius*1.80 + 32.00
 
-	ans[0] = kelvin
-	ans[1] = fahrenheit
+	ans := []float64{kelvin, fahrenheit}
 	return ans
 }
