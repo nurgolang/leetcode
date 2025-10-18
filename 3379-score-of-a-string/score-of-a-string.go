c9a02dabfa6586776d356c29b8151b1c8d037733
nurgolang <9029998@gmail.com>
2025-10-18 16:45:22 +0400

    Time: 0 ms (100.00%) | Memory: 4 MB (49.36%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.go
+++ 3379-score-of-a-string/score-of-a-string.go
@@ -1,8 +1,12 @@
 func scoreOfString(s string) int {
-    var score int
+	var score int
 
-    for i:=0; i < len(s) - 1; i++ {
-        score += int(math.Abs(float64(int(s[i]) - int(s[i+1]))))
-    }
-    return score
+	for i := 0; i < len(s)-1; i++ {
+		diff := int(s[i]) - int(s[i+1])
+		if diff < 0 {
+			diff = -diff
+		}
+        score += diff
+	}
+	return score
 }
